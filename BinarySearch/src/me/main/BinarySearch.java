3550a91ab16398268408d8659147675e7fc9c357
Mo <mo@pc>
2020-09-29 12:25:10 +0200

    time measurement added

--- BinarySearch/src/me/main/BinarySearch.java
+++ BinarySearch/src/me/main/BinarySearch.java
@@ -1,14 +1,77 @@
 package me.main;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class BinarySearch {
 	public static void main(String[] args) {
-		int[] myIntArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-		for (int n : myIntArray) {
-			System.out.println(iterativeSearch(myIntArray, n));
-			System.out.println(binarySearchIterative(myIntArray, n));
-			System.out.println(binarySearchRecursive(myIntArray, n));
-			System.out.println();
+		System.out.println("Start...");
+		keepJavaOccupied(1111);
+		System.out.println("Waited...");
+
+		measureTime(1_000, 50_000_000, 10);
+	}
+	
+	public static void keepJavaOccupied(int ms) {
+		long start = System.currentTimeMillis();
+		while (true) {
+			if (System.currentTimeMillis() - ms > start) {
+				return;
+			}
 		}
+	}
+	
+	public static int[] getRandomIntArr(int arraySize, int maxNumSize) {
+		int[] arr = new int[arraySize];
+		Random r = new Random();
+		for (int i = 0; i < arraySize; i++) {
+			arr[i] = r.nextInt(maxNumSize);
+		}
+		Arrays.sort(arr);
+		return arr;
+	}
+	
+	public static void measureTime(int iterations, int arraySize, int maxNumSize) {
+		int[] arrToSearch = getRandomIntArr(arraySize, maxNumSize);
+		int[] numbersToSearch = new int[iterations];
+		Random r = new Random();
+		for (int i = 0; i < numbersToSearch.length; i++) {
+			if (r.nextInt(101) < 90) {
+				numbersToSearch[i] = arrToSearch[r.nextInt(arrToSearch.length)];
+			} else {
+				numbersToSearch[i] = r.nextInt(maxNumSize); 
+			}
+		}
+		
+		// iterative
+		double start = System.nanoTime();
+		for(int toSearch : numbersToSearch) {
+			iterativeSearch(arrToSearch, toSearch);
+		}
+		double iterativeTime = System.nanoTime() - start;
+		
+		System.out.println("Time took iterative Search: " + iterativeTime);
+		
+		// binarySearch iterative
+		start = System.nanoTime();
+		for(int toSearch : numbersToSearch) {
+			binarySearchIterative(arrToSearch, toSearch);
+		}
+		double binarySearchIterativeTime = System.nanoTime() - start;
+		
+		System.out.println("Time took binary search Iterative Time: " + binarySearchIterativeTime);
+		
+		System.out.println(iterativeTime / binarySearchIterativeTime);
+		/*
+		// binarySearch Recursive
+		start = System.nanoTime();
+		for(int toSearch : numbersToSearch) {
+			binarySearchRecursive(arrToSearch, toSearch);
+		}
+		double binarySearchRecursiveTime = System.nanoTime() - start;
+		
+		System.out.println("Time took binary search Recursive Time: " + binarySearchRecursiveTime);
+		*/
 	}
 
 	public static int iterativeSearch(int[] arr, int search) {
