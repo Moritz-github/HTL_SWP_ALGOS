1830028d0a603397517e784cb72aad1d55d140c0
Moritz-github <47506586+Moritz-github@users.noreply.github.com>
2020-12-03 19:10:31 +0100

    fibonacci

--- Rekursion/src/me/main/Fakultauet.java
+++ Rekursion/src/me/main/Fakultauet.java
@@ -5,17 +5,16 @@ import java.util.Random;
 
 public class Fakultauet {
 	public static void main(String[] args){
-			Scanner reader = new Scanner(System.in);
-			int n = reader.nextInt();
+		Scanner reader = new Scanner(System.in);
+		int n = reader.nextInt();
 			
-			System.out.println(fibonacci_recursive(i));
+		System.out.println(fibonacci_recursive(5));
 	}
 
-	public static int faculty(int n, int factor){
-        if (n == 0) {
-            return 1;
-        } else {
-            return base*getFactor(base,factor-1);
-        }
+	public static int faculty(int n){
+		if (n <= 2) {
+			return n;
+		}
+		return n * faculty(n - 1);
 	}
 }
