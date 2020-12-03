17d5a807340993923be8f6d15ef2a0280c9632cf
Moritz-github <47506586+Moritz-github@users.noreply.github.com>
2020-12-03 18:25:55 +0100

    bubble sort

---
+++ BubbleSort/src/me/main/BubbleSort.java
@@ -0,0 +1,29 @@
+package me.main;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class BubbleSort {
+	public static void main(String[] args) {
+		int[] a = {3,2,6,6,5,9,1}
+		int[] b = bubblesort(a);
+		
+		for(int i : b){
+			System.out.println(i + " ");
+	}
+	
+	public static int[] bubblesort(int[] toSort) {
+		int temp;
+		for(int i=1; i < toSort.length; i++) {
+			for(int j=0; j < toSort.length-i; j++) {
+				if(toSort[j] > toSort[j+1]) {
+					temp = toSort[j];
+					toSort[j] = toSort[j+1];
+					toSort[j+1] = temp;
+				}
+				
+			}
+		}
+		return toSort;
+	}
+}
